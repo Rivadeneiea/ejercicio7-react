12b424346648134a75d72c1a5037de98c36d8487
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-21 15:19:13 -0300

    mapeado el contenido de mi lista de empleados

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import EmpleadoList from "./components/EmpleadoList";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-center mb-3">Empleado list</h1>
+      <EmpleadoList></EmpleadoList>
+    </>
+  );
 }
 
 export default App;
